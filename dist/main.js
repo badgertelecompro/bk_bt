199037d7deda6eb3915d3f589b3bca3a54383aed
Victor Steven Castillo Blanco <victor.castillo@globalmvm.com>
2024-07-18 18:05:44 +0000

    se ajusta cors

--- dist/main.js
+++ dist/main.js
@@ -4,12 +4,30 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const allowedOrigins = [
+        'http://127.0.0.1:8080',
+        'http://localhost:4200',
+        'https://badgertelecompro.netlify.app',
+        'https://bkbt-production.up.railway.app'
+    ];
     app.enableCors({
-        origin: 'http://127.0.0.1:8080',
+        origin: (origin, callback) => {
+            console.log('Origin:', origin);
+            if (!origin)
+                return callback(null, true);
+            if (allowedOrigins.indexOf(origin) !== -1) {
+                callback(null, true);
+            }
+            else {
+                console.error('Not allowed by CORS:', origin);
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
         allowedHeaders: 'Content-Type, Accept, Authorization',
     });
     await app.listen(3000);
+    console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -7,25 +7,29 @@ async function bootstrap() {
   // Lista de orígenes permitidos
   const allowedOrigins = [
     'http://127.0.0.1:8080',
-    'http://localhost:4200', // Añade aquí otros orígenes permitidos
-    'https://bkbt-production.up.railway.app/api/action/find',
+    'http://localhost:4200',
+    'https://badgertelecompro.netlify.app', // Añade aquí el origen correcto
     'https://bkbt-production.up.railway.app'
   ];
 
   // Habilitar CORS
   app.enableCors({
     origin: (origin, callback) => {
+      console.log('Origin:', origin); // Agrega este log para verificar el origen de la solicitud
       // Permitir solicitudes sin origen (por ejemplo, desde herramientas como Postman)
       if (!origin) return callback(null, true);
       if (allowedOrigins.indexOf(origin) !== -1) {
         callback(null, true);
       } else {
+        console.error('Not allowed by CORS:', origin); // Agrega este log para identificar orígenes no permitidos
         callback(new Error('Not allowed by CORS'));
       }
     },
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
+
   await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
